c7dd456d806550ed6b03c89a7b904958ebf0dce8
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2024-10-27 23:57:29 -0300

    config: criando estrutura de routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#000706',
+        brand: {
+          300: '#00272D',
+          200: '#00272D',
+          100: '#0C7E7E',
+        },
+        cream: {
+          200: '#BFAC8B',
+          100: '#F2F1EC',
+        },
+        light: '#E5E5E5',
+        delete: '#9B0303',
+      },
+    },
   },
   plugins: [],
-}
+};
