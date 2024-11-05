eb09dfeefa6e621a4ffe5b09430c8aac4e2792d1
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2024-11-05 17:36:03 -0300

    feat: criando abstração para requisições a api

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,7 @@ export default [
       sourceType: 'module',
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
     },
     plugins: {
---
+++ src/lib/httpService.ts
@@ -0,0 +1,79 @@
+// httpService.ts
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+class HttpService {
+  private client: AxiosInstance;
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: import.meta.env.VITE_API_URL as string, // URL base da API
+      timeout: 10000, // tempo limite em milissegundos (ajuste conforme necessário)
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // Interceptor para adicionar cabeçalhos ou tokens
+    this.client.interceptors.request.use(
+      (config: InternalAxiosRequestConfig) => {
+        // Exemplo de como adicionar um token de autenticação
+        const token = localStorage.getItem('token'); // ou qualquer outro armazenamento seguro
+        if (token && config.headers) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    // Interceptor de resposta para lidar com erros globalmente
+    this.client.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error) => {
+        console.error('Erro na requisição:', error);
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  // Método GET
+  public get<T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.client.get(url, config);
+  }
+
+  // Método POST
+  public post<T>(
+    url: string,
+    data: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.client.post(url, data, config);
+  }
+
+  // Método PUT
+  public put<T>(
+    url: string,
+    data: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.client.put(url, data, config);
+  }
+
+  // Método DELETE
+  public delete<T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.client.delete(url, config);
+  }
+}
+
+export default HttpService;
