86678d2809aadbb1ccbe6ebf146d03cfda2bd49a
Mateus Romani <mateusromani@gmail.com>
2024-11-13 12:46:11 -0300

    adicionando dinâmica para os botões de inscrição e login

--- src/pages/sign_up/index.tsx
+++ src/pages/sign_up/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { Button } from '../../components/Button';
 
-export const Login = () => {
+export const SignupPage = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <div className="flex flex-col w-screen absolute top-0 left-0 h-full bg-brand-300 px-16 place-content-center">
       <div className="flex flex-col h-18"></div>
@@ -10,10 +15,20 @@ export const Login = () => {
         className="flex flex-col pt-2 pb-24 w-max h-max inset-0 m-auto"
       >
         <div id="tabs" className="w-full">
-          <button className="w-60 h-12.5 text-cream-100 text-xs">
-            Inscreva-se
-          </button>
-          <button className="w-60 h-12.5 text-cream-100 text-xs">Login</button>
+          <Link to="/sign_up">
+            <button
+              className={`w-60 h-12.5 text-cream-100 text-xs ${isActive('/sign_up') ? 'border-b-2' : ''}`}
+            >
+              Inscreva-se
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              className={`w-60 h-12.5 text-cream-100 text-xs ${isActive('/') ? 'border-b-2' : ''}`}
+            >
+              Login
+            </button>
+          </Link>
         </div>
 
         <div
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Users } from './pages/users';
-import { Login } from './pages/login';
+import { SignupPage } from './pages/sign_up';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<SignupPage />} />
+      <Route path="/sign_up" element={<SignupPage />} />
       <Route path="/home" element={<Home />} />
       <Route path="*" element={<h1>Not Found</h1>} />
       <Route path="/users" element={<Users />} />
