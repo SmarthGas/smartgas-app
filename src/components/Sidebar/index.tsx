7e422989597862495dee0c079d507651f71b8a22
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2024-11-25 15:54:58 -0300

    feat: inserindo logica de redirecionamento em rotas privadas

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,7 @@
 import { Info, Note, Package, Users } from '@phosphor-icons/react';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Button } from '../Button';
 import { NavItem } from './NavItem';
-import { useUser } from '../../providers/userContext';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -10,11 +9,6 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
-  const { user, setUser } = useUser();
-
-  useEffect(() => {
-    setUser({ id: '1', name: 'John Doe', email: 'john@email.com' });
-  }, []);
   return (
     <div
       className={`fixed top-0 left-0 w-64 h-full bg-brand-400 shadow-xl text-white transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Users } from './pages/users';
+import { useUser } from './providers/userContext';
+
+interface PrivateRoutesProps {
+  children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRoutesProps> = ({ children }) => {
+  const { user } = useUser();
+  if (!user) {
+    console.log('User not logged in');
+    return <Navigate to="/login" />;
+  }
+  return <>{children}</>;
+};
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<h1>Not Found</h1>} />
-      <Route path="/users" element={<Users />} />
+
+      <Route
+        path="/users"
+        element={
+          <PrivateRoute>
+            <Users />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 };
