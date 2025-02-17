436f219b667c28f069b923d51a519050840075bc
Mateus Romani <mateusromani@gmail.com>
2025-02-17 18:38:53 -0300

    Remover temporariamente o componente de "Inscrever-se"

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
-import { SignUpForm } from './SignUpForm';
+import React from 'react';
 import { SignInForm } from './SignInForm';
 
 export const Login = () => {
   // Resgata a localização atual da página para definir a aba ativa
-  const [loginMode, setLoginMode] = useState<'sign_up' | 'sign_in'>('sign_up');
 
   return (
     <div className="flex flex-col w-screen absolute top-0 left-0 h-full bg-brand-300 px-16 items-center p-[10vh]">
@@ -12,38 +10,18 @@ export const Login = () => {
         id="content"
         className="flex flex-col pt-2 pb-24 w-max h-max inset-0 space-y-8"
       >
-        <div id="tabs" className="w-full">
-          <button
-            className={`w-60 h-12.5 text-cream-100 text-xs ${loginMode === 'sign_up' ? 'border-b-2' : ''}`}
-            onClick={() => setLoginMode('sign_up')}
-          >
-            Inscrever-se
-          </button>
-          <button
-            className={`w-60 h-12.5 text-cream-100 text-xs ${loginMode === 'sign_in' ? 'border-b-2' : ''}`}
-            onClick={() => setLoginMode('sign_in')}
-          >
-            Login
-          </button>
-        </div>
+        <div id="tabs" className="w-full"></div>
 
         <div
           id="section_title"
           className="text-cream-100 text-4xl font-semibold"
         >
-          <p>{loginMode === 'sign_up' ? 'Inscrever-se' : 'Login'}</p>
+          <p>Login</p>
         </div>
 
-        {loginMode === 'sign_up' && (
-          <div>
-            <SignUpForm />
-          </div>
-        )}
-        {loginMode === 'sign_in' && (
-          <div>
-            <SignInForm />
-          </div>
-        )}
+        <div>
+          <SignInForm />
+        </div>
       </div>
       <div className="h-18"></div>
     </div>
