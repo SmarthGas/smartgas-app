176e5f646a6f0fb4625fb13c44db18940e65f9b3
Pedro Barauna <pedrobarauna.tech@gmail.com>
2025-03-25 18:31:47 -0300

    edit supplier and edit client forms using formik

--- src/pages/clients/formEditClient.tsx
+++ src/pages/clients/formEditClient.tsx
@@ -4,6 +4,23 @@ import { Input } from '../../components/Input';
 import { ClientType } from '../../types/client';
 import { useSnackbar } from 'notistack';
 import api from '../../lib/api';
+import * as Yup from 'yup';
+import { removeMask } from '../../utils/removeMask';
+import { Form, Formik } from 'formik';
+import { InputForm } from '../../components/InputForm';
+import { validateCNPJ } from '../../utils/validateCNPJ';
+
+interface FormValues {
+  name: string;
+  email: string;
+  cnpj: string;
+  zipcode: string;
+  street: string;
+  streetNumber: string;
+  complement: string;
+  ddd: string;
+  cellphone: string;
+}
 
 interface FormEditClientProps {
   selectedClient: ClientType;
@@ -13,6 +30,45 @@ interface FormEditClientProps {
   setClients: React.Dispatch<React.SetStateAction<ClientType[]>>;
 }
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  email: Yup.string().required(),
+  cnpj: Yup.string()
+    .nullable()
+    .notRequired()
+    .test('len', 'CNPJ deve ter 14 caracteres numéricos', (value) => {
+      if (!value) return true; // Se o campo estiver vazio, não valida
+      const cleanValue = removeMask(value);
+      return cleanValue.length === 14;
+    })
+    .test('valid-cnpj', 'CNPJ Inválido', (value) => {
+      if (!value) return true; // Se o campo estiver vazio, não valida
+      const cleanValue = removeMask(value);
+      return validateCNPJ(cleanValue);
+    }),
+
+  zipcode: Yup.string()
+    .nullable()
+    .notRequired()
+    .test('len', 'zipcode deve ter 8 caracteres numéricos', (value) => {
+      if (!value) return true; // Se o campo estiver vazio, não valida
+      const cleanValue = removeMask(value);
+      return cleanValue.length === 8;
+    }),
+  street: Yup.string().nullable().notRequired(),
+  streetNumber: Yup.string().nullable().notRequired(),
+  complement: Yup.string().nullable().notRequired(),
+  ddd: Yup.string().nullable().notRequired(),
+  cellphone: Yup.string()
+    .nullable()
+    .notRequired()
+    .test('len', 'Telefone deve ter 9 caracteres numéricos', (value) => {
+      if (!value) return true; // Se o campo estiver vazio, não precisa validar
+      const cleanValue = removeMask(value);
+      return cleanValue.length === 9;
+    }),
+});
+
 export const FormEditClient = ({
   setClients,
   selectedClient,
@@ -20,27 +76,45 @@ export const FormEditClient = ({
 }: FormEditClientProps) => {
   const { enqueueSnackbar } = useSnackbar();
 
-  const [editClientData, setEditClientData] =
-    useState<ClientType>(selectedClient);
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const initialValues: FormValues = {
+    name: selectedClient.name,
+    email: selectedClient.email,
+    cnpj: selectedClient.cnpj,
+    zipcode: selectedClient.zipcode,
+    street: selectedClient.street,
+    streetNumber: selectedClient.streetNumber,
+    complement: selectedClient.complement,
+    ddd: selectedClient.ddd,
+    cellphone: selectedClient.cellphone,
+  };
+
+  const handleSubmit = async (values: FormValues) => {
     try {
-      const patchClientBody = {
-        name: editClientData.name,
-        email: editClientData.email,
-        zipcode: editClientData.zipcode,
-        cnpj: editClientData.cnpj,
-        street: editClientData.street,
-        streetNumber: editClientData.streetNumber,
-        complement: editClientData.complement,
-        ddd: editClientData.ddd,
-        cellphone: editClientData.cellphone,
-      };
-      await api.patch(`/client/${selectedClient.id}`, patchClientBody);
+      if (values.cnpj) values.cnpj = removeMask(values.cnpj);
+      if (values.zipcode) values.zipcode = removeMask(values.zipcode);
+      if (values.cellphone) values.cellphone = removeMask(values.cellphone);
+
+      const cleanValues = Object.entries(values).reduce(
+        (acc: Record<string, any>, [key, value]) => {
+          if (value !== null && value !== undefined && value !== '') {
+            acc[key] = value;
+          }
+          return acc;
+        },
+        {}
+      );
+
+      const { data }: { data: { updatedClient: ClientType } } = await api.patch(
+        `/client/${selectedClient.id}`,
+        cleanValues
+      );
+
+      const { updatedClient } = data;
+
       setClients((prev) =>
         prev.map((client) => {
           if (client.id === selectedClient.id) {
-            return { ...client, ...editClientData };
+            return { ...client, ...updatedClient };
           }
           return client;
         })
@@ -53,144 +127,102 @@ export const FormEditClient = ({
     }
   };
   return (
-    <form className="flex flex-col flex-wrap  gap-4" onSubmit={handleSubmit}>
-      <div className="flex flex-col gap-4 flex-wrap max-h-[200px]">
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Nome:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.name}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                name: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Email:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.email}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                email: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>CEP:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.zipcode}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                zipcode: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>CNPJ:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.cnpj}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                cnpj: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Rua:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.street}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                street: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Número:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.streetNumber}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                streetNumber: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Complemento:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.complement}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                complement: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>DDD:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.ddd}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                ddd: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="flex gap-4">
-          <label htmlFor="" className="flex justify-end w-[100px] text-xs">
-            <p>Telefone:</p>{' '}
-          </label>
-          <Input
-            value={editClientData.cellphone}
-            onChange={(e) =>
-              setEditClientData({
-                ...editClientData,
-                cellphone: e.target.value,
-              })
-            }
-          />
-        </div>
-      </div>
-      <div className="flex w-full justify-end gap-4 pt-4">
-        <Button label="Salvar" icon="save" type="submit" />
-        <Button
-          label="Cancelar"
-          icon="trash"
-          variant="delete"
-          onClick={() => setModal({ title: '', name: '' })}
-        />
-      </div>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting, isValid, values, handleChange }) => (
+        <Form className="flex flex-col flex-wrap  gap-4">
+          <div className="flex flex-col gap-4 flex-wrap max-h-[200px]">
+            <InputForm
+              id="name"
+              name="name"
+              type="text"
+              label="Nome"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="cnpj"
+              name="cnpj"
+              type="text"
+              label="CNPJ"
+              mask="99.999.999/9999-99"
+              value={values.cnpj}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="zipcode"
+              name="zipcode"
+              type="text"
+              label="zipcode"
+              mask="99999-999"
+              value={values.zipcode}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="street"
+              name="street"
+              type="text"
+              label="Logradouro"
+              value={values.street}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="streetNumber"
+              name="streetNumber"
+              type="text"
+              label="Número"
+              value={values.streetNumber}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="complement"
+              name="complement"
+              type="text"
+              label="Complemento"
+              value={values.complement}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="ddd"
+              name="ddd"
+              type="text"
+              label="DDD"
+              value={values.ddd}
+              onChange={handleChange}
+            />
+            <InputForm
+              id="cellphone"
+              name="cellphone"
+              type="text"
+              label="Telefone"
+              mask="99999-9999"
+              value={values.cellphone}
+              onChange={handleChange}
+            />{' '}
+          </div>
+          <div className="flex w-full justify-end gap-4 pt-4">
+            <Button
+              label="Salvar"
+              icon="save"
+              type="submit"
+              loading={isSubmitting}
+              disabled={!isValid || isSubmitting}
+            />
+            <Button label="Cancelar" icon="trash" variant="delete" />
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
